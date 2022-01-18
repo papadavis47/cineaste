25807ed7f75edca42be36dc12b29abf1536c678d
John William Davis <jwdavisdeveloper@gmail.com>
2022-01-17 20:42:15 -0800

    Create markup for MovieList

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -3,8 +3,24 @@ import Movie from "../Movie";
 
 const MovieList = () => {
   return (
-    <div>
-      <h1>This is a test from Movie List</h1>
+    <div className='flex flex-col w-1/2 p-4 mt-5 bg-blue-900 border-4 border-sky-200'>
+      {/* Line below will be replaced with a prop eventually -title to the list*/}
+      <div className='text-white'>
+        <h1 className='mb-3 text-4xl font-bold text-center text-white'>Movies I Love</h1>
+        <h2 className='mb-3 ml-3 text-2xl italic max-width-fit'>
+          This is the list of movies that mean the most to me. Nos is the time for all good men to
+          come to the aid
+        </h2>
+      </div>
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <div>
+        <p className='my-3 ml-3 text-2xl italic text-white max-width-fit'>
+          "This is the time for all good men to come to the aid of the party."
+        </p>
+      </div>
     </div>
   );
 };
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,14 +1,15 @@
 import React from "react";
-import MovieForm from "../components/MovieForm/MovieForm.js";
-import ListForm from "../components/ListForm/ListForm.js";
-import Movie from "../components/Movie/Movie.js";
+// import MovieForm from "../components/MovieForm/MovieForm.js";
+// import ListForm from "../components/ListForm/ListForm.js";
+// import Movie from "../components/Movie/Movie.js";
+import MovieList from "../components/MovieList/MovieList.js";
 
 const Test = () => {
   return (
     <div className='flex flex-col items-center'>
       {/* <MovieForm /> */}
       {/* <ListForm /> */}
-      <Movie />
+      <MovieList />
     </div>
   );
 };
