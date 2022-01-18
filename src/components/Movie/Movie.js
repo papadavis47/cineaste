e70155550d63993f3f2d66e8e598748cc907b003
John William Davis <jwdavisdeveloper@gmail.com>
2022-01-17 20:12:08 -0800

    Create markup for Movie component

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,9 +1,32 @@
 import React from "react";
 
+const movie = [
+  {
+    name: "Platoon",
+    director: "Oliver Stone",
+    genre: "Drama",
+    memories: "Seeing it for the first time with my Dad in '87 before Mom died",
+    decadeCreated: "1980s",
+  },
+];
+
 const Movie = () => {
+  const { name, director, genre, memories, decadeCreated } = movie[0];
   return (
-    <div>
-      <p>this is a single movie component</p>
+    <div className='flex flex-col p-5 m-2 text-lg text-white border-4 rounded-md bg-sky-700 border-sky-300'>
+      <h1 className='text-2xl font-bold'>{name}</h1>
+      <h2>
+        Directed by: <span className='font-bold'>{director}</span>
+      </h2>
+      <p>
+        Genre: <span className='font-bold'>{genre}</span>
+      </p>
+      <p>
+        Decade Created: <span className='font-bold'>{decadeCreated}</span>
+      </p>
+      <p>
+        Memories: "<span className='italic'>{memories}</span>"
+      </p>
     </div>
   );
 };
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,12 +1,14 @@
 import React from "react";
 import MovieForm from "../components/MovieForm/MovieForm.js";
 import ListForm from "../components/ListForm/ListForm.js";
+import Movie from "../components/Movie/Movie.js";
 
 const Test = () => {
   return (
     <div className='flex flex-col items-center'>
-      <MovieForm />
-      <ListForm />
+      {/* <MovieForm /> */}
+      {/* <ListForm /> */}
+      <Movie />
     </div>
   );
 };
